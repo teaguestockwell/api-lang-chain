9ab35f9524e66cce63798a3d189c5b1cee156ec9
teaguestockwell <71202372+teaguestockwell@users.noreply.github.com>
2023-09-14 14:57:12 -0700

    Dont verify env var are defined twice

--- e2e.js
+++ e2e.js
@@ -1,38 +1,29 @@
 import { create } from './dist/index.module.js';
 import { config } from 'dotenv';
 
-const getEnv = () => {
-  config();
-  const azureOpenAiKey = process.env.AZURE_OPENAI_KEY;
-  const azureOpenAiEndpoint = process.env.AZURE_OPENAI_ENDPOINT;
-  const deploymentId = process.env.DEPLOYMENT_ID;
-  if (!azureOpenAiKey || !azureOpenAiEndpoint || !deploymentId) {
-    throw 'invalid env';
-  }
-  return { azureOpenAiKey, azureOpenAiEndpoint, deploymentId };
-};
-
-const logger = (...args) => {
-  if (args[0] === 'create') {
-    for (let i = 1; i < args.length; i++) {
-      try {
-        console.log(JSON.stringify(JSON.parse(args[i]), null, 2));
-      } catch {
-        console.log(args[i]);
-      }
-    }
-  }
-};
-
 const main = async () => {
+  config();
   const client = await create({
-    ...getEnv(),
-    logger,
+    azureOpenAiKey: process.env.AZURE_OPENAI_KEY,
+    azureOpenAiEndpoint: process.env.AZURE_OPENAI_ENDPOINT,
+    deploymentId: process.env.DEPLOYMENT_ID,
     openApiUrl:
       'https://raw.githubusercontent.com/oasis-tcs/odata-openapi/main/examples/Northwind-V3.openapi3.json',
+    logger: (...args) => {
+      if (args[0] === 'create') {
+        for (let i = 1; i < args.length; i++) {
+          try {
+            console.log(JSON.stringify(JSON.parse(args[i]), null, 2));
+          } catch {
+            console.log(args[i]);
+          }
+        }
+      }
+    },
   });
 
-  const prompt = 'write me a haiku with category names that have the letter a in them';
+  const prompt =
+    'write me a haiku with category names that have the letter a in them';
   const res = await client.promptApiChat(prompt);
   console.log(JSON.stringify(res, null, 2));
 };
